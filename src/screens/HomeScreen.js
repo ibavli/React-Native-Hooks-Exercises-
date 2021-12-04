1e637c80490dbcb2217c5a7136cabc799b58cda2
ibavli <ibrahim.bavli35@gmail.com>
2021-12-04 16:26:37 +0300

    homescreen was created

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+
+const HomeScreen = () => {
+  return <Text>Hello RN!</Text>
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
